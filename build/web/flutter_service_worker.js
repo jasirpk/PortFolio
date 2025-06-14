e221bccc01b4d17c6b91c3abc1aa2b43d62c80e0
Jasir PK <jasir@tathkarah.in>
2025-06-14 15:46:20 +0530

    deploy3

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -56,7 +56,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "ec37c15871cc189e996fe8c5bf967665
 "canvaskit/skwasm.wasm": "1c93738510f202d9ff44d36a4760126b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "6badf58bf84ca7c2854ee008cdac15b8",
+"flutter_bootstrap.js": "99e5ee13b62aa9435624a8973a88ffc6",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
